eeafe8782580e3627457e262202e1e965bfcb7ff
Evangelos Zotos <evangelos.zotos@cgi.com>
2019-03-09 12:15:09 +0100

    refactoring!

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,42 @@ const gulp = require("gulp");
 const gulpCopy = require("gulp-copy");
 const path = require("path");
 const shell = require("shelljs");
-
-const config = require("./deployer.config.json");
-
 const opn = require("opn");
+var _mergeWith = require("lodash.mergewith");
+
+const defaultConfig = require("./deployer.config.default.json");
+const userConfig = require("./deployer.config.json");
+
+function keepUserArrayCustomizer(objValue, srcValue) {
+  if (Array.isArray(srcValue)) {
+    return srcValue;
+  }
+}
+
+if (userConfig.serverRoot && !userConfig.server) {
+  userConfig.server = {
+    root: userConfig.serverRoot
+  };
+}
+
+if (userConfig.clientRoot && !userConfig.client) {
+  userConfig.client = {
+    root: userConfig.clientRoot
+  };
+}
+
+const config = _mergeWith(defaultConfig, userConfig, keepUserArrayCustomizer);
+
+const appNamePlaceHolder = ":appName";
+const clientRootPlaceHolder = ":root";
 
 function setEnvironment(config, url) {
   config.appName = url;
+}
+
+const rootWorkingDirectory = shell.pwd();
+function cdRoot() {
+  shell.cd(rootWorkingDirectory);
 }
 
 Object.keys(config.env).forEach(environmentKey => {
@@ -17,14 +46,6 @@ Object.keys(config.env).forEach(environmentKey => {
     setEnvironment(config, config.env[environmentKey]);
     done();
   });
-});
-
-// set dev url as default
-//setEnvironment(config, config.env.dev);
-
-gulp.task("kk", done => {
-  console.log(process.argv);
-  done();
 });
 
 gulp.task("check-gitUrl", done => {
@@ -36,62 +57,101 @@ gulp.task("check-gitUrl", done => {
 });
 
 gulp.task("clean", done => {
+  cdRoot();
   shell.rm("-rf", config.server.root);
   done();
 });
 
 gulp.task("copy-server", () => {
-  //const serverFiles = ["./server/lib/**/*", "./server/public/**/*"];
+  cdRoot();
   const serverFiles = [
-    "./server/**/*",
-    "./server/.gitignore",
+    `${config.server.root}**/*`,
     "!server/node_modules/**/*"
   ];
 
   return gulp
     .src(serverFiles)
-    .pipe(gulpCopy(config.server.root, { prefix: 1 }));
+    .pipe(gulpCopy(config.buildRoot, { prefix: 1 }));
 });
 
+function renderTemplate(tmpl, placeholder, value) {
+  return tmpl.replace(placeholder, value);
+}
+
 gulp.task("copy-client", () => {
-  const clientFiles = ["./client/build/**/*"];
+  cdRoot();
+
+  let clientFiles = [];
+
+  if (config.client) {
+    if (config.client.precopy) {
+      clientFiles.push(
+        `${renderTemplate(
+          config.client.public,
+          clientRootPlaceHolder,
+          config.client.root
+        )}/**/*`
+      );
+    }
+
+    if (config.client.exclude) {
+      config.client.exclude.forEach(excludePattern => {
+        clientFiles.push(`!**/*${excludePattern}`);
+      });
+    }
+  }
+
+  console.log(clientFiles);
+
   const publicFolder = path.join(config.server.root, config.server.publicPath);
 
   return gulp.src(clientFiles).pipe(gulpCopy(publicFolder, { prefix: 2 }));
 });
 
-gulp.task("build-client", done => {
-  shell.cd("client");
-  shell.exec("npm run build");
-  shell.cd("..");
+gulp.task("precopy-client", done => {
+  cdRoot();
+  if (config.client) {
+    if (config.client.precopy) {
+      config.client.precopy.forEach(command => {
+        command[1] = command[1].replace(
+          clientRootPlaceHolder,
+          config.client.root
+        );
+
+        shell[command[0]](command[1]);
+      });
+    }
+  }
+
   done();
 });
 
 gulp.task(
   "build",
-  gulp.series("clean", "copy-server", "build-client", "copy-client")
+  gulp.series("clean", "copy-server", "precopy-client", "copy-client")
 );
 
 gulp.task("git-init", done => {
-  const gitUrlPattern =
-    config.gitUrlPattern || "https://git.heroku.com/:appName.git";
+  cdRoot();
 
-  const gitUrl = gitUrlPattern.replace(":appName", config.appName);
+  const gitUrl = config.gitUrlPattern.replace(
+    appNamePlaceHolder,
+    config.appName
+  );
 
   shell.cd(config.server.root);
 
   shell.exec("git init");
   shell.exec(`git remote add heroku ${gitUrl}`);
 
-  shell.cd("..");
   done();
 });
 
 function createApp() {
   console.log("creating: " + config.appName);
-  const v = shell.exec(`heroku create ${config.gitUrl}`);
+  const herokuCreateResult = shell.exec(`heroku create ${config.appName}`);
 
-  if (v.stderr.includes("already taken")) {
+  if (herokuCreateResult.stderr.includes("already taken")) {
     console.log("EXISTS ALREADY!");
   } else {
     console.log("Created!");
@@ -99,7 +159,7 @@ function createApp() {
 }
 
 gulp.task("git-push", done => {
-
+  cdRoot();
   createApp();
 
   shell.cd(config.server.root);
@@ -108,7 +168,6 @@ gulp.task("git-push", done => {
   shell.exec("git commit -m deplyment");
   shell.exec("git push --set-upstream heroku master --force");
 
-  shell.cd("..");
   done();
 });
 
@@ -117,11 +176,27 @@ gulp.task(
   gulp.series("check-gitUrl", "build", "git-init", "git-push", "clean")
 );
 
-gulp.task("open", done => {
-  const webUrlPattern =
-    config.webUrlPattern || "https://:appName.herokuapp.com";
+gulp.task(
+  "local",
+  gulp.series("build", done => {
+    cdRoot();
+    shell.cd(config.server.root);
 
-  const webUrl = webUrlPattern.replace(":appName", config.appName);
+    console.log("Run: npm install");
+    shell.exec("npm install");
+
+    console.log("starting local build!");
+    shell.exec("npm start");
+
+    done();
+  })
+);
+
+gulp.task("open", done => {
+  const webUrl = config.webUrlPattern.replace(
+    appNamePlaceHolder,
+    config.appName
+  );
 
   console.log("opening: " + webUrl);
   opn(webUrl, { wait: false });
@@ -133,5 +208,3 @@ gulp.task("print-gitUrl", done => {
 
   done();
 });
-
-//git.heroku.com/zotopia-power1.git
