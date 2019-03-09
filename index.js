6d212366f76edaf5cba72992af0848a76558bc4e
Evangelos Zotos <evangelos.zotos@cgi.com>
2019-03-09 20:04:33 +0100

    some cleanups

---
+++ index.js
@@ -0,0 +1,15 @@
+#! /usr/bin/env node
+const packageName = require("./package.json").name;
+const shell = require("shelljs");
+const argv = require("yargs").argv;
+const userConfigFileName = "deployer.config.json";
+const pwd = shell.pwd().toString();
+
+const userConfig = require(`${pwd}/${userConfigFileName}`);
+
+const args = argv._.join(" ");
+
+shell.env["DEPENDENT_STARTUP_PATH"] = pwd;
+shell.cd(`node_modules/${packageName}`);
+
+shell.exec(`npm run gulp ${args}`);
